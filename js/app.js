0cc5977a6810ba4a391a6e3fe9dc89f24b3543fc
galyverasi <galyverasi@gmail.com>
2021-12-22 20:11:14 -0700

    searching for images only once

--- js/app.js
+++ js/app.js
@@ -1,42 +1,61 @@
-
-// create a variables needed
-const input = document.querySelector("input")
-
-const requestUrl = "http://www.reddit.com/search.json?q="
-
-
-// function that cycles through the links in the array and assigns them to source
-document.addEventListener("DOMContentLoaded", ()=>{
-    
-    form.addEventListener("submit", (e)=> {
+const requestURL = 'https://www.reddit.com/search.json?q='
+let resultIndex = 0
+document.addEventListener('DOMContentLoaded', () => {
+    // hide slideshow
+    slideshow.style.display = 'none'
+    searchForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        // fetch data
-        fetch(requestUrl+input.value)
-        .then ((responseData) =>{
-            console.log(responseData)
-            return responseData.json()
-        })
-        .then((jsonData)=>{
-            console.log(jsonData)
-                // imageArray.push(jsonData.data.children[i].data.thumbnail)
-                // console.log(jsonData.data.children)
-                let imageArray = []
-                jsonData.data.children.forEach(imageResults) 
-                console.log(imageArray)   
-                //     // imageArray.push(results.data.thumbnail)
-                //     // data.children[9].data.author
-                // })
-                // console.log(imageArray)
-                // // how do I retrieve thumbnails
-                // // forEach, filter, map
+        // fetch data from reddit
+        // fetchRedditData()
+        const searchTerm = userInput.value.replaceAll(' ', '+')
+        fetch(`http://www.reddit.com/search.json?q=${searchTerm}&limit=100&raw_json=1`)
+            .then((responseData) => {
+                return responseData.json()
             })
-        // write a function to clear the form
-        .catch((error)=>{
-            console.log("Error")
-            console.log(error)
-        })
-    })
-})
-const imageResults = imgObj => {
+            .then((jsonData) => {
+                // do whatever we need to do with the data
+                // console.log(jsonData)
+                // the array of search results
+                const resultsArray = jsonData.data.children
+                // console.log(resultsArray[0].data.url)
+                const imgURLs = resultsArray.map((resultObj) => {
+                    if (resultObj.data.preview) {
+                        return resultObj.data.preview.images[0].source.url
+                    } else {
+                        return 'no preview image'
+                    }
+                })
+                const filteredURLs = imgURLs.filter((url) => {
+                    if (url === 'no preview image') {
+                        return false
+                    } else {
+                        return true
+                    }
+                })
+                for (let i = 0; i < filteredURLs.length; i++) {
+                    // create a new img element
+                    let redditPhoto = document.createElement("img")
+                    // add the src from array
+                    redditPhoto.setAttribute('src', imgURLs[i])
+                    // append img to dom
+                    testImagesList.appendChild(redditPhoto)
 
-}
+                        // const changeSrc = () => {
+                        //     if (resultIndex<filteredURLs.length){
+                        //         slideshow.setAttribute('src', filteredURLs[resultIndex])
+                        //         resultIndex++
+                        //     } else {
+                        //         resultIndex=0
+                        //     }
+                        // }
+                        // changeSrc()
+                        // slideshow.style.display = "block"
+                        // const changeSlide = setInterval(changeSrc, 3000)
+                }
+            })
+                        .catch((error) => {
+                            console.log('Error retrieving data from reddit:')
+                            console.log(error)
+                        })
+                })
+})
---
+++ plan.md
@@ -0,0 +1,9 @@
+# Reddit Slideshow
+
+## 
+
+* create the files needed and make them talk to each other 
+* set up HTML with title, description, form, stop button, and an unordered list for initial image test results (will configure actual slideshowlater)
+*set event listener on search form to fetch data from reddit API
+cleansed data to have an array of preview image urls
+display the images using URLs as the src
